5630935f8cb834e7c7baa2616a964951c55fd0cb
Koh Guan Zeh <kohguanzeh@gmail.com>
2024-08-17 01:52:57 +0800

    Refactor code

--- src/webserver.c
+++ src/webserver.c
@@ -4,23 +4,77 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
-#define HTTP_PORT "80"
-
-WSADATA wsaData;
+int init_winsock();
+int init_server_socket(SOCKET);
+int close_server_socket(SOCKET);
 
 int main() {
-  int wsaResult;
+  if (init_winsock()) {
+    return 1;
+  }
 
-  // Initialize Winsock
-  wsaResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+  SOCKET server_socket = INVALID_SOCKET;
+  if (init_server_socket(server_socket)) {
+    return 1;
+  }
+
+  // Accept client socket
+  SOCKET client_socket = INVALID_SOCKET;
+  client_socket = accept(server_socket, NULL, NULL);
+  if (client_socket == INVALID_SOCKET) {
+    printf("accept failed: %d\n", WSAGetLastError());
+    closesocket(server_socket);
+    WSACleanup();
+    return 1;
+  }
+
+  // Shutdown the send half of the connection since no more data will be sent
+  int wsaResult = shutdown(client_socket, SD_SEND);
+  if (wsaResult == SOCKET_ERROR) {
+    printf("shutdown failed: %d\n", WSAGetLastError());
+    closesocket(client_socket);
+    WSACleanup();
+    return 1;
+  }
+  closesocket(client_socket);
+
+  // Close socket after implementation
+  closesocket(server_socket);
+
+  WSACleanup();
+
+  return 0;
+}
+
+/**
+ * @brief Initializes Winsock library with WSAStartup.
+ *
+ * @return Returns 0 on success, 1 on failure.
+ */
+int init_winsock() {
+  WSADATA wsaData;
+  int wsaResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
   if (wsaResult != 0) {
     printf("WSAStartup failed: %d\n", wsaResult);
     return 1;
   }
 
-  // Create socket for server
-  struct addrinfo *result = NULL, *ptr = NULL, hints;
+  return 0;
+}
 
+/**
+ * @brief Initializes the server listening socket.
+ *
+ * @param listen_socket SOCKET to initialize the server listening socket.
+ * @return Returns 0 on success, 1 on failure.
+ */
+int init_server_socket(SOCKET server_socket) {
+  char const *const HTTP_PORT = "80";
+
+  struct addrinfo *addr_info = NULL;
+
+  struct addrinfo hints;
   ZeroMemory(&hints, sizeof(hints));
   hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_STREAM;
@@ -28,7 +82,7 @@ int main() {
   hints.ai_flags = AI_PASSIVE;
 
   // Resolve the local address and port to be used by the server
-  wsaResult = getaddrinfo(NULL, HTTP_PORT, &hints, &result);
+  int wsaResult = getaddrinfo(NULL, HTTP_PORT, &hints, &addr_info);
   if (wsaResult != 0) {
     printf("getaddrinfo failed: %d\n", wsaResult);
     WSACleanup();
@@ -36,60 +90,37 @@ int main() {
   }
 
   // Create socket for server to listen for client connections
-  SOCKET listenSocket = INVALID_SOCKET;
-  listenSocket =
-      socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+  server_socket = socket(addr_info->ai_family, addr_info->ai_socktype,
+                         addr_info->ai_protocol);
 
-  if (listenSocket == INVALID_SOCKET) {
+  if (server_socket == INVALID_SOCKET) {
     printf("Error at socket(): %ld\n", WSAGetLastError());
-    freeaddrinfo(result);
+    freeaddrinfo(addr_info);
     WSACleanup();
     return 1;
   }
 
   // Setup the TCP listening socket
-  wsaResult = bind(listenSocket, result->ai_addr, (int)result->ai_addrlen);
+  wsaResult =
+      bind(server_socket, addr_info->ai_addr, (int)addr_info->ai_addrlen);
   if (wsaResult == SOCKET_ERROR) {
     printf("bind failed with error: %d\n", WSAGetLastError());
-    freeaddrinfo(result);
-    closesocket(listenSocket);
+    freeaddrinfo(addr_info);
+    closesocket(server_socket);
     WSACleanup();
     return 1;
   }
+
   // Free memory allocated by getaddrinfo()
-  freeaddrinfo(result);
+  freeaddrinfo(addr_info);
 
   // Listen on a socket
-  if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR) {
+  if (listen(server_socket, SOMAXCONN) == SOCKET_ERROR) {
     printf("Listen failed with error: %ld\n", WSAGetLastError());
-    closesocket(listenSocket);
+    closesocket(server_socket);
     WSACleanup();
     return 1;
   }
-
-  // Accept client socket
-  SOCKET clientSocket = INVALID_SOCKET;
-  clientSocket = accept(listenSocket, NULL, NULL);
-  if (clientSocket == INVALID_SOCKET) {
-    printf("accept failed: %d\n", WSAGetLastError());
-    closesocket(listenSocket);
-    WSACleanup();
-    return 1;
-  }
-
-  // Close socket after implementation
-  closesocket(listenSocket);
-
-  // Shutdown the send half of the connection since no more data will be sent
-  wsaResult = shutdown(clientSocket, SD_SEND);
-  if (wsaResult == SOCKET_ERROR) {
-    printf("shutdown failed: %d\n", WSAGetLastError());
-    closesocket(clientSocket);
-    WSACleanup();
-    return 1;
-  }
-  closesocket(clientSocket);
-  WSACleanup();
 
   return 0;
 }
