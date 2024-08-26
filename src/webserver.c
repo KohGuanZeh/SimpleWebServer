08b59e7a8a74c9184126d9d063501c938deb2370
Koh Guan Zeh <kohguanzeh@gmail.com>
2024-08-26 12:01:58 +0800

    Refactor connection and request handling

--- src/webserver.c
+++ src/webserver.c
@@ -4,8 +4,12 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
+#define BUFFER_LEN 512
+
 int init_winsock();
 int init_server_socket(SOCKET *);
+int handle_connection(SOCKET *);
+int handle_request(SOCKET *);
 
 int main() {
   if (init_winsock()) {
@@ -17,60 +21,13 @@ int main() {
     return EXIT_FAILURE;
   }
 
-  // Accept client socket
-  SOCKET client_socket = INVALID_SOCKET;
-  client_socket = accept(server_socket, NULL, NULL);
-  if (client_socket == INVALID_SOCKET) {
-    printf("accept failed: %d\n", WSAGetLastError());
-    closesocket(server_socket);
-    WSACleanup();
-    return EXIT_FAILURE;
-  }
-
-  // Following recv specification of int
-  const int BUFFER_LEN = 512;
-  char recv_buffer[BUFFER_LEN];
-  int recv_result;
-  int send_result;
-
-  // Receive until the peer shuts down the connection
-  do {
-    recv_result = recv(client_socket, recv_buffer, BUFFER_LEN, 0);
-    if (recv_result > 0) {
-      printf("Bytes received: %d\n", recv_result);
-
-      // Echo the buffer back to the sender
-      send_result = send(client_socket, recv_buffer, recv_result, 0);
-      if (send_result == SOCKET_ERROR) {
-        printf("send failed: %d\n", WSAGetLastError());
-        closesocket(client_socket);
-        WSACleanup();
-        return 1;
-      }
-      printf("Bytes sent: %d\n", send_result);
-    } else if (recv_result == 0) {
-      printf("Client connection closing...\n");
-    } else {
-      printf("recv failed: %d\n", WSAGetLastError());
-      closesocket(client_socket);
-      WSACleanup();
-      return 1;
+  while (TRUE) {
+    if (handle_connection(&server_socket)) {
+      break;
     }
-
-  } while (recv_result > 0);
-
-  // Shutdown the send half of the connection since no more data will be sent
-  int wsaResult = shutdown(client_socket, SD_SEND);
-  if (wsaResult == SOCKET_ERROR) {
-    printf("shutdown failed: %d\n", WSAGetLastError());
-    closesocket(client_socket);
-    WSACleanup();
-    return EXIT_FAILURE;
   }
 
-  closesocket(client_socket);
   closesocket(server_socket);
-
   WSACleanup();
 
   return EXIT_SUCCESS;
@@ -96,7 +53,7 @@ int init_winsock() {
 /**
  * @brief Initializes the server listening socket.
  *
- * @param listen_socket SOCKET to initialize the server listening socket.
+ * @param server_socket `SOCKET *` to initialize the server listening socket.
  * @return Returns 0 on success, 1 on failure.
  */
 int init_server_socket(SOCKET *server_socket) {
@@ -151,6 +108,78 @@ int init_server_socket(SOCKET *server_socket) {
     WSACleanup();
     return 1;
   }
+
+  return 0;
+}
+
+/**
+ * @brief Handle connection established by clients.
+ *
+ * @param server_socket `SOCKET *` for the listening server socket.
+ */
+int handle_connection(SOCKET *server_socket) {
+  SOCKET client_socket = INVALID_SOCKET;
+  client_socket = accept(*server_socket, NULL, NULL);
+  if (client_socket == INVALID_SOCKET) {
+    printf("accept failed: %d\n", WSAGetLastError());
+    return 1;
+  }
+
+  if (handle_request(&client_socket)) {
+    // Client Socket cleanup done on error.
+    // Each time request fails, server will stop.
+    // Todo: Create an enum to check if server socket should close.
+    return 1;
+  }
+
+  // Shutdown the send half of the connection since no more data will be sent
+  int wsaResult = shutdown(client_socket, SD_SEND);
+  if (wsaResult == SOCKET_ERROR) {
+    printf("shutdown failed: %d\n", WSAGetLastError());
+  }
+
+  closesocket(client_socket);
+  WSACleanup();
+  return 0;
+}
+
+/**
+ * @brief Handle request of client socket
+ *
+ * @param client_socket `SOCKET *` for the client socket.
+ * @return Returns 0 on success, 1 on failure.
+ */
+int handle_request(SOCKET *client_socket) {
+  // Following recv specification of int
+  char recv_buffer[BUFFER_LEN];
+  int recv_result;
+  int send_result;
+
+  // Receive until the peer shuts down the connection
+  do {
+    recv_result = recv(*client_socket, recv_buffer, BUFFER_LEN, 0);
+    if (recv_result > 0) {
+      printf("Bytes received: %d\n", recv_result);
+
+      // Echo the buffer back to the sender
+      send_result = send(*client_socket, recv_buffer, recv_result, 0);
+      if (send_result == SOCKET_ERROR) {
+        printf("send failed: %d\n", WSAGetLastError());
+        closesocket(*client_socket);
+        WSACleanup();
+        return 1;
+      }
+      printf("Bytes sent: %d\n", send_result);
+    } else if (recv_result == 0) {
+      printf("Client connection closing...\n");
+    } else {
+      printf("recv failed: %d\n", WSAGetLastError());
+      closesocket(*client_socket);
+      WSACleanup();
+      return 1;
+    }
+
+  } while (recv_result > 0);
 
   return 0;
 }
